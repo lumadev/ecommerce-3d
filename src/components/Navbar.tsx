ad4b6db16ffe1681359d5608c3a3da9219c785cd
Luma Macagnan <lumamacagnan@gmail.com>
2026-03-30 18:55:36 -0300

    move LoginModal to be a module

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 
 import SearchDialog from "../features/search/SearchDialog";
-import LoginModal from "./LoginModal";
+import LoginModal from "../features/auth/LoginModal";
 
 const Navbar = () => {
   const { totalItems, setIsOpen } = useCart();
