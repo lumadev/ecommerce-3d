2c99eb7b861e9d99aff7424e00c8f42ab10bbe0a
Luma Macagnan <lumamacagnan@gmail.com>
2026-04-08 20:26:21 -0300

    fix typescript error in getErrorMessage

--- src/infra/http/httpError.ts
+++ src/infra/http/httpError.ts
@@ -1,9 +1,11 @@
-export function getErrorMessage(error: any): string {
-  if (error.response?.data?.message) {
+import { AxiosError } from "axios";
+
+export function getErrorMessage(error: unknown): string {
+  if (error instanceof AxiosError && error.response?.data?.message) {
     return error.response.data.message;
   }
 
-  if (error.message) {
+  if (error instanceof Error && error.message) {
     return error.message;
   }
 
