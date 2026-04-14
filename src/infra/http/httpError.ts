a28c12f32803633ff8f2b50c51546da979158e49
Luma Macagnan <lumamacagnan@gmail.com>
2026-04-14 12:39:43 -0300

    set portuguese api error messages to the user

--- src/infra/http/httpError.ts
+++ src/infra/http/httpError.ts
@@ -1,13 +1,46 @@
-import { AxiosError } from "axios";
+import axios from "axios";
+
+function getHttpStatusMessage(status?: number): string {
+  switch (status) {
+    case 400:
+      return "Não foi possível concluir a solicitação. Revise os dados e tente novamente.";
+    case 401:
+      return "E-mail ou senha inválidos.";
+    case 403:
+      return "Você não tem permissão para realizar esta ação.";
+    case 404:
+      return "O recurso solicitado não foi encontrado.";
+    case 409:
+      return "Não foi possível concluir a solicitação porque os dados informados já existem.";
+    case 422:
+      return "Os dados informados são inválidos.";
+    case 429:
+      return "Muitas tentativas em pouco tempo. Aguarde e tente novamente.";
+    default:
+      if (status && status >= 500) {
+        return "Ocorreu um erro interno. Tente novamente mais tarde.";
+      }
+
+      return "Não foi possível concluir a solicitação.";
+  }
+}
 
 export function getErrorMessage(error: unknown): string {
-  if (error instanceof AxiosError && error.response?.data?.message) {
-    return error.response.data.message;
+  if (axios.isAxiosError(error)) {
+    if (error.code === "ERR_NETWORK" || !error.response) {
+      return "Não foi possível conectar ao servidor. Verifique sua internet e tente novamente.";
+    }
+
+    if (error.code === "ECONNABORTED") {
+      return "A solicitação demorou mais do que o esperado. Tente novamente.";
+    }
+
+    return getHttpStatusMessage(error.response.status);
   }
 
-  if (error instanceof Error && error.message) {
-    return error.message;
+  if (error instanceof Error) {
+    return "Ocorreu um erro ao processar sua solicitação. Tente novamente.";
   }
 
-  return "Erro inesperado";
+  return "Erro inesperado. Tente novamente.";
 }
